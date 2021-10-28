3182e111b8572f5d4ddd4eb383b2c46796449efb
Deveus <raffree.dev@gmail.com>
2021-10-29 01:37:35 +0300

    NftSeries

--- upgrade-nft-component/core/src/lib.rs
+++ upgrade-nft-component/core/src/lib.rs
@@ -1,14 +1,18 @@
-use near_contract_standards::non_fungible_token::core::NonFungibleTokenCore;
+use std::collections::HashMap;
+use near_contract_standards::non_fungible_token::metadata::TokenMetadata;
+use near_sdk::collections::LookupMap;
 use near_contract_standards::non_fungible_token::NonFungibleToken;
 use near_contract_standards::non_fungible_token::{Token, TokenId};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::UnorderedSet;
-use near_sdk::json_types::ValidAccountId;
+use near_sdk::collections::UnorderedMap;
+
 use near_sdk::Balance;
 use near_sdk::Gas;
 use near_sdk::{
     env, ext_contract, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, Promise,
-    PromiseOrValue, PromiseResult
+    PromiseOrValue, PromiseResult,
+    
 };
 
 #[derive(BorshSerialize, BorshStorageKey)]
@@ -38,6 +42,33 @@ pub struct AirdropReward {
     account_id: AccountId,
     amount: Balance,
 }
-pub trait NftEngine{
-   fn airdrop(&mut self, rewards: AirdropRewards);
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct AppContract {
+    token: NonFungibleToken,
+    owner: AccountId,
+    pending_ft_rewards: LookupMap<AccountId, Balance>,
+}
+pub trait SupportsAirdrop{
+ fn airdrop(&mut self, rewards: AirdropRewards);
+ fn add_pending_ft_rewards(&mut self, rewards: Vec<(AccountId, Balance)>);
+ fn get_pending_ft_rewards() -> LookupMap<AccountId, Balance>;
+}
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct LinkDrop {
+    pub accounts: UnorderedMap<AccountId, Balance>,
+}
+pub type NftSeriesId = String;
+
+//Nft Series
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct NftSeries {
+	metadata: TokenMetadata,
+	creator_id: AccountId,
+	tokens: UnorderedSet<TokenId>,
+    price: Option<Balance>,
+    is_mintable: bool,
+    royalty: HashMap<AccountId, u32>
 }
