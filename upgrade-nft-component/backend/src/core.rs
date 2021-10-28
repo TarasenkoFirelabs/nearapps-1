248eba5edb2b57d850c87ffd881f7b225c00b882
Deveus <raffree.dev@gmail.com>
2021-10-28 13:39:30 +0300

    app contract, core items

---
+++ upgrade-nft-component/backend/src/core.rs
@@ -0,0 +1,44 @@
+use near_contract_standards::non_fungible_token::core::NonFungibleTokenCore;
+use near_contract_standards::non_fungible_token::NonFungibleToken;
+use near_contract_standards::non_fungible_token::{Token, TokenId};
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::UnorderedSet;
+use near_sdk::json_types::ValidAccountId;
+use near_sdk::Balance;
+use near_sdk::Gas;
+use near_sdk::{
+    env, ext_contract, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, Promise,
+    PromiseOrValue, PromiseResult
+};
+
+#[derive(BorshSerialize, BorshStorageKey)]
+enum StorageKey {
+    NonFungibleToken,
+    TokenAccountMapping,
+    TokenMetadata,
+    Enumeration,
+    Approval,
+}
+pub trait Ownable {
+    fn assert_owner(&self) {
+        assert_eq!(
+            env::predecessor_account_id(),
+            self.owner(),
+            "Ownable: predecessor is not the owner"
+        );
+    }
+    fn owner(&self) -> AccountId;
+    fn transfer_ownership(&mut self, owner: AccountId);
+}
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct AirdropRewards(Vec<AirdropReward>);
+
+#[derive(BorshDeserialize, BorshSerialize, Clone)]
+pub struct AirdropReward {
+    account_id: AccountId,
+    amount: Balance,
+}
+
+pub trait NftEngine{
+   fn airdrop(&mut self, rewards: AirdropRewards);
+}
--- upgrade-nft-component/backend/src/lib.rs
+++ upgrade-nft-component/backend/src/lib.rs
@@ -2,3 +2,7 @@ mod airdrop;
 mod locking;
 mod minting;
 mod upgrade;
+mod appcontract;
+mod core;
+
+
