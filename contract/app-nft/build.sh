d0eea2861f57cf674fb4ea6ab5876b7f0d8d9097
Deveus <raffree.dev@gmail.com>
2021-11-04 16:35:52 +0200

    fix build

--- contract/app-nft/build.sh
+++ contract/app-nft/build.sh
@@ -3,5 +3,4 @@ TARGET="${CARGO_TARGET_DIR:-target}"
 set -e
 cd "`dirname $0`"
 cargo build --target wasm32-unknown-unknown --release
-cp $TARGET/wasm32-unknown-unknown/release/app-nft.wasm ./res/
 
