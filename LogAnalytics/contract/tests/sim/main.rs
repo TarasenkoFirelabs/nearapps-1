6e101e2e367cb3a4bef31fe57f2b254fcae69042
NinoLipartiia4ire <n.lipartiia@4irelabs.com>
2021-11-07 23:34:01 +0200

    changes in tests

--- LogAnalytics/contract/tests/sim/main.rs
+++ LogAnalytics/contract/tests/sim/main.rs
@@ -1,7 +1,9 @@
-use near_sdk_sim::{call, view, deploy, init_simulator, ContractAccount, UserAccount};
+use near_sdk_sim::{call, view, deploy, init_simulator, ContractAccount, UserAccount, to_yocto, STORAGE_AMOUNT};
+use api::CallContract;
 //use crate::utils::init;
 use std::str;
-//use near_sdk::serde_json::json;
+use near_sdk::serde_json::json;
+use near_sdk::AccountId;
 
 extern crate base64;
 use base64::encode;
@@ -10,15 +12,40 @@ near_sdk_sim::lazy_static_include::lazy_static_include_bytes! {
     COUNTER_BYTES => "../res/api_call.wasm",
 }
 
+const CONTRACT_ID: &str = "contract";
+
+pub fn init() -> (UserAccount, ContractAccount<CallContract>, UserAccount) {
+    // Use `None` for default genesis configuration; more info below
+    let root = init_simulator(None);
+
+    let contract = deploy!(
+        contract: CallContract,
+        contract_id: CONTRACT_ID,
+        bytes: &COUNTER_BYTES,
+        signer_account: root
+    );
+
+    let alice = root.create_user(
+        "alice".parse().unwrap(),
+        to_yocto("100") // initial balance
+    );
+
+    (root, contract, alice)
+}
+
 #[test]
 fn simulate_log_analytics() {
-    let (root, contract, _, _, _) = init();
+    let (root, contract, _) = init();
 
-    let initial = String::from("ABC_DEFG_1233");
-    let encoded: String = encode(initial);
-    let decoded: String = root.view(
+    let initial = "ABC_DEFG_1233_234".to_string();
+    let encoded: String = encode(initial.clone());
+    let result = call!(
+        root,
         contract.log_analytics(encoded)
     );
+    result.assert_success();
+
+    let decoded: String = (*result.logs()[0]).to_string(); 
 
     assert_eq!(initial, decoded);
 }
