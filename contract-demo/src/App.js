05140caceb05c8dadc65b7770fce1f612ebd9e65
a.tarasenko <a.tarasenko@4irelabs.com>
2021-11-11 20:07:26 +0200

    demo contracts

---
+++ contract-demo/src/App.js
@@ -0,0 +1,64 @@
+import 'regenerator-runtime/runtime';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import RegistrationForm from './pages/registration/Registration';
+import SignIn from './pages/sign-in/SignIn';
+
+const App = ({ contract, currentUser, nearConfig, wallet }) => {
+  useEffect(() => {
+    // 
+  }, []);
+
+  const signIn = () => {
+    wallet.requestSignIn(
+      nearConfig.contractName,
+      'NEAR Apps'
+    );
+  };
+
+  const signOut = () => {
+    wallet.signOut();
+    window.location.replace(window.location.origin + window.location.pathname);
+  };
+
+  return (
+    <main>
+      <header>
+        <h1>NEAR Apps</h1>
+        { currentUser
+          ? <button onClick={signOut}>Log out</button>
+          : <button onClick={signIn}>Log in</button>
+        }
+      </header>
+      { currentUser
+        ? <RegistrationForm currentUser={currentUser} nearConfig={nearConfig} />
+        : <SignIn/>
+      }
+    </main>
+  );
+};
+
+App.propTypes = {
+  contract: PropTypes.shape({
+    call: PropTypes.func.isRequired,
+    log_analytics: PropTypes.func.isRequired
+  }).isRequired,
+  currentUser: PropTypes.shape({
+    accountId: PropTypes.string.isRequired,
+    balance: PropTypes.string.isRequired
+  }),
+  // analytics: PropTypes.shape({
+  //   app_id: PropTypes.string.isRequired,
+  //   action_id: PropTypes.string.isRequired,
+  //   user_name: PropTypes.string.isRequired
+  // }),
+  nearConfig: PropTypes.shape({
+    contractName: PropTypes.string.isRequired
+  }).isRequired,
+  wallet: PropTypes.shape({
+    requestSignIn: PropTypes.func.isRequired,
+    signOut: PropTypes.func.isRequired
+  }).isRequired
+};
+
+export default App;
---
+++ contract-demo/src/pages/registration/Registration.tsx
@@ -0,0 +1,124 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { useAlert } from "react-alert";
+import styles from './Registration.module.sass';
+import OutlinedInput from "../../components/OutlinedInput";
+import Button from "../../components/Button";
+import { Colors } from "../../utils/Colors";
+
+const regExpression = /^[a-zA-Z0-9!#$%^&*()_+\-=\[\]{};':"\\|<>\/?]*$/
+
+const Registration = ({ account, nearConfig, currentUser }) => {
+    const alert = useAlert();
+    const [accountID, setAccountID] = useState<string>('');
+    const [isValid, setIsValid] = useState<boolean>(false)
+
+    const validate = useCallback(() => {
+        let isValid = true;
+        let checkAccountIDReg = regExpression.test(accountID)
+
+        if (!checkAccountIDReg || accountID.length < 2) {
+            isValid = false
+        }
+
+        setIsValid(isValid);
+    },[accountID])
+
+    useEffect(() => {
+        validate()
+    }, [validate])
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        setAccountID(value);
+    }
+
+    const encode64_AccountID = (AccountID) => {
+      return encode_utf8_base64(AccountID);
+    }
+
+    const submitAccountID = async () => {
+      try {
+        const args = {"accountID": encode64_AccountID( accountID )};
+        setIsValid(false)
+
+        callContract(account, nearConfig, 'make_wallet', args)
+          .then((txid) => {
+            const link = `https://explorer.${nearConfig.networkId}.near.org/transactions/${txid}`;
+            alert.show(link);
+
+            // currentUser.accountId = accountID;
+            // account = await near.account(currentUser.accountId);
+
+          })
+          .finally(() => {setIsValid(true)})
+
+      } catch(error) {
+        alert.error(error);
+      }
+    }
+
+    return (
+        <div className={ styles.root }>
+            <div className={ styles.baseBody }>
+                <div className={ styles.registrationUnderBody2 }>
+                  {currentUser.accountId}
+                </div>
+                <hr className={ styles.horizontalLine2 } />
+
+                <div className='flexInlineCenter textHeader padding-10-10-0'>
+                    {'Create NEAR account'}
+                </div>
+                <hr className={ styles.horizontalLine1 } />
+                <div className={ styles.registrationSubBody0 }>
+                    {'Enter an Account ID to use with your NEAR account. Your Account ID will be used for all NEAR operations, ' +
+                    'including sending and receiving assets.\n'}
+                </div>
+                <div className='flexInlineCenter'>
+                     <OutlinedInput onChange={ handleChange } label={'ACCOUNT ID'} />
+                </div>
+                <div className={ styles.registrationSubBody1 }>
+                    {'Your account ID can contain any of the following:\n'}
+                </div>
+
+                <ul className={ styles.registrationListBody1 }>
+                    <li>{ 'Lowercase characters (a-z)'}</li>
+                    <li>{'Digits (0-9)'}</li>
+                    <li>{'Characters (_-) can be used as separators'}</li>
+                </ul>
+                <div className={ styles.registrationSubBody3 }>
+                    {'Your account ID CANNOT contain:'}
+                </div>
+                <ul className={ styles.registrationListBody2 }>
+                    <li>{'Characters "@" or "."'}</li>
+                    <li>{'Fewer than 2 characters'}</li>
+                    <li>{'More than 64 characters (including .near)'}</li>
+                </ul>
+                <div className='flexInlineCenter'>
+                    <Button
+                        onClick={ submitAccountID }
+                        text='Create'
+                        textColor={ Colors.white }
+                        backgroundColor={ Colors.blue }
+                        disabled={ !isValid }
+                    />
+                </div>
+                <div className={ styles.registrationUnderBody1 }>
+                    {'By creating a NEAR account, you agree to the NEAR Wallet'} &#160;
+                    <a
+                      href="https://wallet.testnet.near.org/20210319-near-tcs.a776bb61.pdf"
+                      className={ styles.registrationLink }>
+                      {'Terms of Service'}
+                    </a>
+                  &#160;{'and'} &#160;
+                    <a
+                      href="https://near.org/privacy/"
+                      className={ styles.registrationLink }>
+                      {'Privacy Policy.'}
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Registration;
