538db6806b809e8f501ea2ebb5b4dfc452467cb5
Buckram123 <m.donchenko@4irelabs.com>
2021-11-12 16:43:29 +0200

    added contract-demo

---
+++ contract-demo/src/index.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import App from './App';
+import getConfig from './config.js';
+import * as nearAPI from 'near-api-js';
+
+// Initializing contract
+async function initContract() {
+  const nearConfig = getConfig(process.env.NODE_ENV || 'testnet');
+
+  // Initializing connection to the NEAR TestNet
+  const near = await nearAPI.connect({
+    deps: {
+      keyStore: new nearAPI.keyStores.BrowserLocalStorageKeyStore()
+    },
+    ...nearConfig
+  });
+
+  // Needed to access wallet
+  const walletConnection = new nearAPI.WalletConnection(near);
+
+  // Load in account data
+  let currentUser;
+  if(walletConnection.getAccountId()) {
+    currentUser = {
+      accountId: walletConnection.getAccountId(),
+      balance: (await walletConnection.account().state()).amount
+    };
+
+    currentUser.account = await near.account(currentUser.accountId);
+  }
+
+  // Initializing our contract APIs by contract name and configuration
+  const contract = await new nearAPI.Contract(walletConnection.account(), nearConfig.contractName, {
+    // View methods are read-only – they don't modify the state, but usually return some value
+    viewMethods: [],
+    // Change methods can modify the state, but you don't receive the returned value when called
+    changeMethods: ['call', 'log_analytics'],
+    // Sender is the account ID to initialize transactions.
+    // getAccountId() will return empty string if user is still unauthorized
+    sender: walletConnection.getAccountId()
+  });
+
+  return { contract, currentUser, nearConfig, walletConnection };
+}
+
+window.nearInitPromise = initContract()
+  .then(({ contract, currentUser, nearConfig, walletConnection }) => {
+
+    // optional configuration
+    const options = {
+      position: positions.TOP_RIGHT,
+      timeout: 5000,
+      offset: '30px',
+      transition: transitions.FADE
+    }
+
+    const AlertTemplate = ({ style, options, message, close }) => (
+      <div style={style}>
+        <button onClick={close}>X</button>
+        <div>View this transaction in <a href={message} target="_blank">explorer</a></div>
+      </div>
+    )
+    
+    const Root = () => (
+      <AlertProvider template={AlertTemplate} {...options}>
+      <App
+        contract={contract}
+        currentUser={currentUser}
+        nearConfig={nearConfig}
+        wallet={walletConnection}
+      />
+      </AlertProvider>
+    )
+
+    ReactDOM.render(
+      <Root />,
+      document.getElementById('root')
+    );
+  });
---
+++ contract-demo/src/pages/registration/Registration.tsx
@@ -0,0 +1,153 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { useAlert } from "react-alert";
+import styles from './Registration.module.sass';
+import OutlinedInput from "../../components/OutlinedInput";
+import Button from "../../components/Button";
+import { Colors } from "../../utils/Colors";
+import { generateSeedPhrase } from "near-seed-phrase";
+import callContract from "../../utils/CallContract";
+const CONTRACT_NAME_MAKE_WALLET = process.env.CONTRACT_NAME_MAKE_WALLET;
+const CONTRACT_NAME_NFT = process.env.CONTRACT_NAME_NFT;
+
+const regExpression = /^[a-zA-Z0-9!#$%^&*()_+\-=\[\]{};':"\\|<>\/?]*$/
+
+const Registration = ({ contract, currentUser, nearConfig }) => {
+    const alert = useAlert();
+    const [accountID, setAccountID] = useState<string>('');
+    const [isValid, setIsValid] = useState<boolean>(false)
+
+    const validate = useCallback(() => {
+        let isValid = true;
+        let checkAccountIDReg = regExpression.test(accountID)
+
+        if (!checkAccountIDReg || accountID.length < 2) {
+            isValid = false
+        }
+
+        setIsValid(isValid);
+    },[accountID])
+
+    useEffect(() => {
+        validate()
+    }, [validate])
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        setAccountID(value);
+    }
+
+    const submitAccountID = async () => {
+      try {
+        console.log('Generate key pair from random on curve type: ed25519');
+
+        const { seedPhrase, publicKey } = generateSeedPhrase();
+        // const keyPair = KeyPair.fromRandom('ed25519');
+        // const publicKey = keyPair.getPublicKey().toString();
+
+        console.log('Generate seedPhrase: ', seedPhrase);
+        console.log('Generate publicKey: ', publicKey);
+
+        const newAccount =  {
+          account_id: accountID,
+          public_key: publicKey ///"AngnS4YGwMGoMDwnyNK3wXt8f9xYfXfvGfKvXhYC1AVF",
+        };
+
+        const args = {
+          "tags": [{"person": "Raf", "company": "4ire", "purpose": "test"}],
+          "contract_name": CONTRACT_NAME_MAKE_WALLET,
+          "args": {"function_name": "new_wallet", "params": JSON.stringify({"new_account": newAccount})  }
+        };
+
+        console.log(contract);
+
+        setIsValid(false);
+
+        const result = await contract.call({"args": args});
+
+        console.log(result);
+
+        setIsValid(true)
+
+        // callContract(currentUser.account, nearConfig, 'app', args)
+        //   .then((txid) => {
+        //     console.log(txid);
+        //     const link = `https://explorer.${nearConfig.networkId}.near.org/transactions/${txid}`;
+        //     alert.show(link);
+        //
+        //     // currentUser.accountId = accountID;
+        //     // account = await near.account(currentUser.accountId);
+        //
+        //   })
+        //   .finally(() => {setIsValid(true)})
+
+      } catch(error) {
+        console.log(error);
+        alert.error(error);
+      }
+    }
+
+    return (
+        <div className={ styles.root }>
+            <div className={ styles.baseBody }>
+                <div className={ styles.registrationUnderBody2 }>
+                  You are: {currentUser.accountId}
+                </div>
+                <hr className={ styles.horizontalLine2 } />
+
+                <div className='flexInlineCenter textHeader padding-10-10-0'>
+                    {'Create NEAR account'}
+                </div>
+                <hr className={ styles.horizontalLine1 } />
+                <div className={ styles.registrationSubBody0 }>
+                    {'Enter an Account ID to use with your NEAR account. Your Account ID will be used for all NEAR operations, ' +
+                    'including sending and receiving assets.\n'}
+                </div>
+                <div className='flexInlineCenter'>
+                     <OutlinedInput onChange={ handleChange } label={'ACCOUNT ID'} />
+                </div>
+                <div className={ styles.registrationSubBody1 }>
+                    {'Your account ID can contain any of the following:\n'}
+                </div>
+
+                <ul className={ styles.registrationListBody1 }>
+                    <li>{ 'Lowercase characters (a-z)'}</li>
+                    <li>{'Digits (0-9)'}</li>
+                    <li>{'Characters (_-) can be used as separators'}</li>
+                </ul>
+                <div className={ styles.registrationSubBody3 }>
+                    {'Your account ID CANNOT contain:'}
+                </div>
+                <ul className={ styles.registrationListBody2 }>
+                    <li>{'Characters "@" or "."'}</li>
+                    <li>{'Fewer than 2 characters'}</li>
+                    <li>{'More than 64 characters (including .near)'}</li>
+                </ul>
+                <div className='flexInlineCenter'>
+                    <Button
+                        onClick={ submitAccountID }
+                        text='Create'
+                        textColor={ Colors.white }
+                        backgroundColor={ Colors.blue }
+                        disabled={ !isValid }
+                    />
+                </div>
+                <div className={ styles.registrationUnderBody1 }>
+                    {'By creating a NEAR account, you agree to the NEAR Wallet'} &#160;
+                    <a
+                      href="https://wallet.testnet.near.org/20210319-near-tcs.a776bb61.pdf"
+                      className={ styles.registrationLink }>
+                      {'Terms of Service'}
+                    </a>
+                  &#160;{'and'} &#160;
+                    <a
+                      href="https://near.org/privacy/"
+                      className={ styles.registrationLink }>
+                      {'Privacy Policy.'}
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Registration;
