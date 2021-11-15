bdb21814a2b90e4a3562b182fed8aa4f82cc3f47
Buckram123 <m.donchenko@4irelabs.com>
2021-11-15 14:38:47 +0200

    fast fix, no need to use full-config.js, maybe remove it?

--- contract-demo/src/config.js
+++ contract-demo/src/config.js
@@ -58,4 +58,16 @@ function getConfig(env) {
   }
 }
 
-module.exports = getConfig;
+function getContractsConfig(env) {
+  const Config = getConfig(env);
+  const dataContracts = {
+    GAS: "200000000000000",
+    ATTACHED_DEPOSIT_AMOUNT: "1000000000000000000000000", /// 1 Ⓝ
+    CONTRACT_NAME_MAKE_WALLET: process.env.CONTRACT_NAME_MAKE_WALLET,
+    CONTRACT_NAME_NFT: process.env.CONTRACT_NAME_NFT
+  };
+
+  return {...Config, ...dataContracts}
+}
+
+module.exports = getContractsConfig;
