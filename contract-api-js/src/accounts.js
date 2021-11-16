4e3d0edc36f8ff2e3c7155b69492de0aa8cb8559
adsick <adsick@protonmail.com>
2021-11-16 12:53:01 +0200

    generating seed phrases in accounts.js

--- contract-api-js/src/accounts.js
+++ contract-api-js/src/accounts.js
@@ -1,9 +1,57 @@
 const chalk = require("chalk");
+const { keyStores, connect, Contract } = require("near-api-js");
+const { generateSeedPhrase } = require("near-seed-phrase");
+const fs = require("fs");
+const path = require("path");
+const homedir = require("os").homedir();
+
+const CREDENTIALS_DIR = ".near-credentials";
+const ACCOUNT_ID = "adsick.testnet";
+const MAKE_WALLET_CONTRACT_ID = "adsick.testnet";
+const credentialsPath = path.join(homedir, CREDENTIALS_DIR);
+const keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
+
+
+
+const config = {
+    keyStore,
+    networkId: "testnet",
+    nodeUrl: "https://rpc.testnet.near.org",
+};
+
+async function callCreateWallet(accountId, publicKey) {
+    const near = await connect(config);
+    const account = await near.account(ACCOUNT_ID);
+    const contract = new Contract(account, MAKE_WALLET_CONTRACT_ID, {
+        // name of contract you're connecting to
+        viewMethods: [], // view methods do not change state but usually return a value
+        changeMethods: ["makeWallets"], // change methods modify state
+        sender: account, // account object to initialize and sign transactions.
+    });
+
+    const result = await contract.makeWallets(
+        {
+            new_account: { "accountId": accountId, "publicKey": publicKey }, // argument name and value - pass empty object if no args required
+
+            gas: 300000000000000, // attached GAS (optional)
+            amount: 1000000000000000000000000 // attached deposit in yoctoNEAR (optional)
+        }
+    );
+
+    console.log(result);
+}
+
 
 const createWallet = (resp) => {
-  console.log(chalk.greenBright(`Wallet ${resp.wallet} has been successfully created... `));
+
+    //TODO: Seed phrase generation here
+    const { seedPhrase, publicKey } = generateSeedPhrase();
+    console.log(chalk.gray(`SeedPhrase:`) + chalk.bgGray(seedPhrase))
+    callCreateWallet(resp.wallet, resp.publicKey);
+
+    console.log(chalk.greenBright(`Wallet ${resp.wallet} has been successfully created... `));
 };
 
 module.exports = {
-  createWallet,
+    createWallet,
 };
